391a94d092f94eba6e6f0e8913f21f349d4f797c
vagrant <vagrant@devprojects>
2015-10-16 22:00:52 +0000

    First Commit Laravel

---
+++ database/migrations/2015_10_16_214326_create_fondas_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFondasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('fondas', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('address');
+            $table->integer('postalcode');
+            $table->string('schedules');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('fondas');
+    }
+}
